e4a8f76b157a155eb4da4e6838288a3f86c890d7
VinceVT <vincevutruong@gmail.com>
2020-04-09 19:18:22 -0700

    SpringBoot Rest

---
+++ rest/src/test/java/com/cmpe172/irated/rest/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpe172.irated.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
