348fbcf31a4a7ede9ef35d9420efac569e973d4d
Ahmed Darkazanli <adarkazanli2@gmail.com>
2020-04-26 13:15:38 -0700

    Improve search bar and suggestions

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import { fade, withStyles } from '@material-ui/core/styles';
+import { fade, withStyles } from "@material-ui/core/styles";
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
 
-import Suggestions from '../Suggestions';
+import Suggestions from "../Suggestions";
 
 const useStyles = (theme) => ({
   grow: {
@@ -19,46 +19,46 @@ const useStyles = (theme) => ({
     marginRight: theme.spacing(2),
   },
   title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
     },
   },
   search: {
-    position: 'relative',
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
+    "&:hover": {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(3),
-      width: 'auto',
+      width: "auto",
     },
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   inputRoot: {
-    color: 'inherit',
+    color: "inherit",
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
 });
@@ -67,8 +67,9 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchInput: '',
+      searchInput: "",
       results: [],
+      showSuggestions: false,
     };
   }
 
@@ -79,13 +80,13 @@ class Header extends React.Component {
 
   getProfessors = () => {
     axios
-      .get('http://localhost:8080/dynamoDb', {
+      .get("http://localhost:8080/dynamoDb", {
         params: {
           name: this.state.searchInput.toLowerCase(),
         },
       })
       .then(({ data }) => {
-        console.log('this is the data', data);
+        console.log("this is the data", data);
         this.setState({
           results: data,
         });
@@ -112,12 +113,16 @@ class Header extends React.Component {
                   root: classes.inputRoot,
                   input: classes.inputInput,
                 }}
-                inputProps={{ 'aria-label': 'search' }}
+                inputProps={{ "aria-label": "search" }}
                 onChange={this.handleChange}
                 value={this.state.searchInput}
+                onClick={() => this.setState({ showSuggestions: true })}
               />
-              {this.state.searchInput.length ? (
-                <Suggestions results={this.state.results} />
+              {this.state.searchInput.length && this.state.showSuggestions ? (
+                <Suggestions
+                  hide={() => this.setState({ showSuggestions: false })}
+                  results={this.state.results}
+                />
               ) : null}
             </div>
           </Toolbar>
--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -14,10 +14,25 @@ const useStyles = (theme) => ({
 });
 
 class TypographyMenu extends React.Component {
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClick, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClick, false);
+  }
+
+  handleClick = (e) => {
+    if (this.node.contains(e.target)) {
+      return;
+    }
+    this.props.hide();
+  };
+
   render() {
     const { results, classes } = this.props;
     return (
-      <Paper className={classes.root}>
+      <Paper className={classes.root} ref={(node) => (this.node = node)}>
         <MenuList>
           {results.length ? (
             results.map((professor) => {
@@ -27,11 +42,15 @@ class TypographyMenu extends React.Component {
                 .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
                 .join(" ");
               return (
-                <MenuItem key={professor.professorId}>
-                  <Link to={`/view-reviews/${professor.professorId}`}>
+                <Link
+                  to={`/view-reviews/${professor.professorId}`}
+                  onClick={() => this.props.hide()}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  <MenuItem key={professor.professorId}>
                     {professor.professorName}
-                  </Link>
-                </MenuItem>
+                  </MenuItem>
+                </Link>
               );
             })
           ) : (
